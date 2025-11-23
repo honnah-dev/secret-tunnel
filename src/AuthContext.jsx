ec60cc985321d60a4153352b655ba18c679638f9
Honnah Weber <lakeside486@protonmail.com>
2025-11-22 20:19:35 -0700

    created username and authentication functions for the user to pass thruogh the game.

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -8,11 +8,51 @@ export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
 
+async function signup(username) {
+  try {
+    const response = await fetch(API + "/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json", },
+      body: JSON.stringify({username}),
+    });
+  const result = await response.json();
+  console.log(result, "\n");
+
+  setToken(result.token);
+  setLocation("TABLET");
+
+  } catch (e) {
+    console.error("oh no :(");
+  }
+  
+}
+async function authenticate() {
+  if (!token) {
+    throw Error("No token found")
+  }
+  try {
+    const response = await fetch(API + "/authenticate", {
+      method: "GET",
+      headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+    });
+  const result = await response.json();
+  console.log(result);
+
+  setToken(result.token);
+  setLocation("TUNNEL");
+
+  } catch (e) {
+    console.error("authentication failed", e);
+    throw e;
+  }
+}
+
+
   // TODO: signup
 
   // TODO: authenticate
 
-  const value = { location };
+  const value = { token, location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
